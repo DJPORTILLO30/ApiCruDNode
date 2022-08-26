4ec7b98326ffacb63a856b68c3758b14796116e9
dportillo <daniloportillo200030@gmail.com>
2022-08-25 18:47:53 -0600

    Validotor para track creado

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { getItems, createItem } = require("../controllers/tracks");
+const { validatorCreateItem } = require("../validators/tracks");
 const router = express.Router();
 
 //TODO http://localhost/api/tracks GET,POST,DELETE,PUT
 
 router.get("/",getItems);
 
-router.post("/",createItem);
+router.post("/",validatorCreateItem,createItem);
 
 module.exports = router ;
